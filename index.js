384b24d28fcc3fc1ab5ffffa7cea52b98bfc4017
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2017-12-26 04:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push({[item]: Math.floor(Math.random() * 101)});
+  console.log(`${item} has been added to your cart.`);
+  return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 1) {
+    console.log(`In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`)  
+  }else if(cart.length > 0) {
+    var string = '';
+    for(var i = 0; i < cart.length - 1; i++) {
+      string += ` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]},`;
+    }
+    string += ` and ${Object.keys(cart[cart.length - 1])} at $${cart[cart.length - 1][Object.keys(cart[cart.length - 1])]}.`
+    console.log(`In your cart, you have${string}`);
+  } else {
+    console.log('Your shopping cart is empty.');
+  }
 }
 
 function total() {
