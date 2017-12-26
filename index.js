77e4fa69c4766bc5ab6f9bb8e7db4da2cf2bf281
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2017-12-26 04:35:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,5 +59,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
+    cart = [];
+  }
 }
